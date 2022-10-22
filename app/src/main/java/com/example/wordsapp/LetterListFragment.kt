bda7fd60f936b95560d4c3781948439c544979ec
Isaac Miti <ikayz@live.com>
2022-10-22 14:51:45 +0200

    Override onCreate

--- app/src/main/java/com/example/wordsapp/LetterListFragment.kt
+++ app/src/main/java/com/example/wordsapp/LetterListFragment.kt
@@ -5,7 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.wordsapp.databinding.FragmentLetterListBinding
+
+private var _binding: FragmentLetterListBinding? = null
+private val binding get() = _binding!!
 
 class LetterListFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
 
 }
