1969bd82def5bd65b4ab424729085474a1258035
Isaac Miti <ikayz@live.com>
2022-10-22 14:54:14 +0200

    Override onCreateView

--- app/src/main/java/com/example/wordsapp/LetterListFragment.kt
+++ app/src/main/java/com/example/wordsapp/LetterListFragment.kt
@@ -10,11 +10,20 @@ import com.example.wordsapp.databinding.FragmentLetterListBinding
 private var _binding: FragmentLetterListBinding? = null
 private val binding get() = _binding!!
 
+
 class LetterListFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setHasOptionsMenu(true)
     }
 
-
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentLetterListBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
 }
